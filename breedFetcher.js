94bdc1af0ca774665362a642f43b82d9ac7a3ff3
Youssef Ragab <ragab.youssef95@gmail.com>
2022-01-20 18:18:32 -0500

    tests passed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,26 @@
-const args = process.argv.slice(2, process.argv.length);
-// console.log(args[0].slice(0, 4))
-const searchId = args[0].slice(0, 4);
-
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/images/search?breed_ids=' + searchId, (error, response, body) => {
-  if (error) {
-    console.log(error)
-    return
-  }
-  if (body.length === 2) {
-    console.log('empty search')
-    return
-  }
-  const data = JSON.parse(body);
-  // console.log(data)
-  // console.log(typeof data)
-  console.log(data[0].breeds[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName.slice(0, 4), (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    // callback(null, data[0].description);
+    // console.log(data)
+    if (data[0] === undefined) {
+      callback("breed not found")
+    } else {
+      callback(null, data[0].description)
+    }
+    // if (body.length === 2) {
+    //   console.log('empty search');
+    //   return;
+    // }
+    // // console.log(data)
+    // // console.log(typeof data)
+    // console.log(data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
