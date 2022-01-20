9430106e141cc64cbaa4df7ae3058f63c715a537
Youssef Ragab <ragab.youssef95@gmail.com>
2022-01-20 16:29:36 -0500

    added breedFetcher functionality

---
+++ breedFetcher.js
@@ -0,0 +1,16 @@
+const args = process.argv.slice(2, process.argv.length);
+// console.log(args[0].slice(0, 4))
+const searchId = args[0].slice(0, 4);
+
+const request = require('request');
+
+request('https://api.thecatapi.com/v1/images/search?breed_ids=' + searchId, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  } 
+  const data = JSON.parse(body);
+  // console.log(data)
+  // console.log(typeof data)
+  console.log(data[0].breeds[0].description);
+});
